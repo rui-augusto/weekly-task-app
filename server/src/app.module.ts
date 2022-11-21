57284c78c0d4e8d83a8791872458bdc84782c166
Rui Augusto <rui.augusto.drumond@gmail.com>
2022-11-21 20:06:33 -0300

    setting database config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,12 @@
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({}),
+    DatabaseModule,
     TaskModule
   ],
   controllers: [],
