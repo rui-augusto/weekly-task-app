5a4dfc15c572554b10f0f9789734afd42e57765e
rui-augusto <rui.augusto.drumond@gmail.com>
2023-01-04 19:06:04 -0300

    setting new url to  api functions

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -6,7 +6,7 @@ export const getAllTasks = async () => {
   try {
     const res = await axios({
       method: 'GET',
-      url: 'http://localhost:3000/task'
+      url: 'http://localhost:3000/tasks/all'
     });
     return res.data;
   } catch(err) {
@@ -18,7 +18,7 @@ export const completeTask = async (id: number, state: number) => {
   try {
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:3000/task/update/:id',
+      url: 'http://localhost:3000/tasks/update/:id',
       params: { id },
       data: { state }
     });
@@ -32,7 +32,7 @@ export const addTask = async (task: string, description: string) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://localhost:3000/task/create',
+      url: 'http://localhost:3000/tasks/create',
       data: { task, description }
     });
     return res;
